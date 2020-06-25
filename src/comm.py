b2d51338c58176288b47d706d8a6a31c016ec1f2
OmerKonan <konanomer@gmail.com>
2020-06-25 18:36:15 +0300

    Communication module added

---
+++ src/comm.py
@@ -0,0 +1,49 @@
+import serial
+
+class Cummunication:
+    
+    def __init__(self):
+        self.portpath = "/dev/ttyACM1"
+        self.boudrate = 115200
+        self.timeout = 1
+        self. stopbits = 1.5
+        self.msgID = 0 # 0 means there is no message
+                           # 1 means it is LPG
+                           # 2 means it is Smoke
+                           # 3 means it is CO
+    
+    def connect(self):
+        port = serial.Serial(self.portpath, self.boudrate, \
+                             timeout = self.timeout, stopbits = self.stopbits)
+        return port
+            
+    def read_message(self, port):
+        
+        ValueByte = port.readline()
+        ValueStr = str(ValueByte)
+        ValueStr = ValueStr.replace('b', '')
+        ValueStr = ValueStr.replace('\'','')
+        print(ValueStr)
+        return ValueStr
+        
+    def getValue(self, ValueStr):
+        val = 0
+        self.msgID = 0
+        if ValueStr.find("LPG") != -1:
+            ValArr = ValueStr.split(":")[1].split("ppm")
+            val = int(ValArr[0])
+            self.msgID = 1
+            #print("Read LPG:", val)
+        if ValueStr.find("SMOKE") != -1:
+            ValArr = ValueStr.split(":")[1].split("ppm")
+            val = int(ValArr[0])
+            self.msgID = 2
+            #print("Read Smoke:", val)
+        if ValueStr.find("CO") != -1:
+            ValArr = ValueStr.split(":")[1].split("ppm")
+            val = int(ValArr[0])
+            self.msgID = 3
+            #print("Read CO:", val)
+            
+        return val, self.msgID
+
